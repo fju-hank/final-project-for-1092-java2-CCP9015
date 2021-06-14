85177e0afcf9ca5a3e4d04acd77d1bb7120f6886
Junpin <tyuighjk86@gmail.com>
2021-06-14 20:02:53 +0800

    take order and print your order

--- src/main/java/com/fju/Tester.java
+++ src/main/java/com/fju/Tester.java
@@ -1,12 +1,37 @@
 package com.fju;
 
+import com.fju.food.*;
+
 import java.io.*;
+import java.util.Scanner;
 
 public class Tester {
     public static void main(String[] args) {
-       ReadFile readFile = new ReadFile();
-       readFile.print();
+        Menu [] food ={
+                new Beef(), new Calpis(), new Cheese(), new Coke(), new Drumstick(),
+                new Fish(), new FrenchFries(), new GrilledEel(), new Gyoza(), new HanjukuEgg(),
+                new LemonSmoothie(), new MangoPudding(), new MatchaPudding(), new Mixed(), new OnsenTamago(),
+                new Pork(), new Salmon(), new SteamedEggs(), new Tempura(), new Vegetables()
+
+        };
+        Scanner scan = new Scanner(System.in);
+
+        ReadFile readFile = new ReadFile();
+        readFile.print();
+        System.out.println();
+        System.out.println("你今天想吃甚麼");
+        String input = scan.next();
+        for (Menu order : food) {
+            if(order.name(input)){
+                System.out.println(order.getName() + "\t" + order.price());
+                break;
+
+            }
+
+            
+        }
        }
+
 
 
 
--- src/main/java/com/fju/food/Menu.java
+++ src/main/java/com/fju/food/Menu.java
@@ -3,7 +3,7 @@ package com.fju.food;
 public abstract class Menu {
     String id;
     public boolean name(String name){
-        return (id.equalsIgnoreCase(name));
+        return (id.startsWith(name));
     }
     public abstract String getName();
     public abstract int price();
