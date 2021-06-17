2cb502dc76d1514285f085c99aa153fd3a241dec
Junpin <tyuighjk86@gmail.com>
2021-06-17 14:37:27 +0800

    tax id number add

--- src/main/java/com/fju/Receipt.java
+++ src/main/java/com/fju/Receipt.java
@@ -3,23 +3,40 @@ package com.fju;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Receipt {
     public static void main(String[] args) {
 
         int i[] = new int[8];
         Random random = new Random();
-        System.out.println("109年5月-6月");
-        System.out.print("CP-");
-        for (int j = 0; j < 10; j++) {
-            i[j] = random.nextInt(8) + 1;
-            System.out.print(i[j]);
+        System.out.println("需要統編嗎");
+        Scanner scan = new Scanner(System.in);
+        String ans = scan.next();
+        if(ans.equals("Y")){
+            System.out.println("請輸入統一編號 :" );
+            String tax = scan.next();
+            System.out.println("109年5月-6月");
+            System.out.print("CP-");
+            for (int j = 0; j < 10; j++) {
+                i[j] = random.nextInt(8) + 1;
+                System.out.print(i[j]);
+            }
+            String now =
+                    new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Calendar.getInstance().getTime());
+            System.out.println(now);
+            System.out.println("統一編號:" + tax);
+        }if(ans.equals("N")) {
+            System.out.println("109年5月-6月");
+            System.out.print("CP-");
+            for (int j = 0; j < 10; j++) {
+                i[j] = random.nextInt(8) + 1;
+                System.out.print(i[j]);
+            }
+            String now =
+                    new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Calendar.getInstance().getTime());
+            System.out.println(now);
 
         }
-        String now =
-                new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Calendar.getInstance().getTime());
-        System.out.println(now);
-
-
     }
 }
